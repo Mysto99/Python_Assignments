945bbd961800de18f201bb540c4051521f690694
Yusuf Moussa <yusufmoussa96@gmailcom>
2021-01-09 17:15:46 -0500

    Not using tkinter

--- First_python_assignment.py
+++ First_python_assignment.py
@@ -1,55 +1,22 @@
-from tkinter import *
-
-root = Tk()
-root.geometry("500x300")
-root.title("Calculator")
-
-def clear():
-	select.grid_forget()
-	square_button.grid_forget()
-	cube_button.grid_forget()
-	circle_button.grid_forget()
-	sphere_button.grid_forget()
-
 def square():
-	clear()
-	Label(root, text="Enter the dimensions: ").grid(row=0, column=0, columnspan=2)
-	Label(root, text="Length: ").grid(row=1, column=0)
-	Label(root, text="Width: ").grid(row=2, column=0)
-	length = Entry(root).grid(row=1, column=1)
-	width = Entry(root).grid(row=2, column=1)
-	calculate = Button(root, text="Calculate").grid(row=2, column=2)
-	length = length.get()
-	width = width.get()
-	perimeter = 2*(length + width)
+	length = int(input("Length: "))
+	width = int(input("Width: "))
+	perimeter = 2*(length+width)
 	area = length*width
-	Label(root, text=str(perimeter)).grid(row=3, column=0)
-	Label(root, text=str(area)).grid(row=4, column=0)
+	print("Perimeter:", str(perimeter), "Area:", str(area))
 
 def cube():
-	clear()
-
-def circle():
-	clear()
-
-def sphere():
-	clear()
-
+	length = int(input("Length: :"))
+	width = int(input("Width: "))
+	height = int(input("Height: "))
+	volume = length*width*height
+	area = 2*(length*width)+2*(length*height)+2*(width*height)
+	print("Volume:", str(volume), "Area:", str(area))
 
 
-select = Label(root, text="Select a shape:")
-select.grid(row=0, column=0, columnspan=2)
+choice = input("Choose a shape: ")
 
-square_button = Button(root, text="Square", command=square)
-square_button.grid(row=1, column=0)
-
-cube_button = Button(root, text="Cube", command=cube)
-cube_button.grid(row=1, column=1)
-
-circle_button = Button(root, text="Circle", command=circle)
-circle_button.grid(row=1, column=2)
-
-sphere_button = Button(root, text="Sphere", command=sphere)
-sphere_button.grid(row=1, column=3)
-
-root.mainloop()
+if choice == "square":
+	square()
+elif choice == "cube":
+	cube()
