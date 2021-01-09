743e556d33c4bdd6b52ea8e48fc0992515f3b677
Yusuf Moussa <yusufmoussa96@gmailcom>
2021-01-09 17:52:54 -0500

    Assignment Complete

--- First_python_assignment.py
+++ First_python_assignment.py
@@ -13,10 +13,28 @@ def cube():
 	area = 2*(length*width)+2*(length*height)+2*(width*height)
 	print("Volume:", str(volume), "Area:", str(area))
 
+def circle():
+	radius = int(input("Radius: "))
+	area = 3.14*(radius**2)
+	circ = 2*3.14*radius
+	print("Area:", str(area), "Circumference:", str(circ))
+
+def sphere():
+	radius = int(input("Radius: "))
+	area = 4*3.14*(radius**2)
+	volume = 0.75*3.14*(radius**3)
+	print("Area:", str(area), "Volume:", str(volume))
 
 choice = input("Choose a shape: ")
+choice = choice.lower()
 
 if choice == "square":
 	square()
 elif choice == "cube":
 	cube()
+elif choice == "circle":
+	circle()
+elif choice == "sphere":
+	sphere()
+else:
+	print("Not Valid")
